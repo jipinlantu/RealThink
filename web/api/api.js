bccac69c7d07525ed36c05f0fcf53f6b9cdacaf2
171945608 <51847595+171945608@users.noreply.github.com>
2021-12-31 11:10:03 +0800

    Add files via upload

---
+++ web/api/api.js
@@ -0,0 +1,15 @@
+var api={
+    "login":'http://real-think.jmwl51.com/admin/index/login',//登录
+    "admin_auth_admin_index":'/admin/auth.admin/index',//管理员管理
+    "admin_auth_admin_add":'/admin/auth.admin/add',//
+    "admin_auth_admin_del":'/admin/auth.admin/del',
+    "admin_auth_group_index":'/admin/auth.group/index',//管理员角色管理
+    "admin_auth_group_add":'/admin/auth.group/add',
+    "admin_auth_group_del":'/admin/auth.group/del',
+    "admin_auth_rule_index":'/admin/auth.rule/index',//管理员菜单管理
+    "admin_auth_rule_add":'/admin/auth.rule/add',
+    "admin_auth_rule_del":'/admin/auth.rule/del',
+    "admin_general_Configs_del":'/admin/general.Configs/index',//配置列表
+    "upload":'/admin/Ajaxs/upload',//配置列表
+}
+
---
+++ web/api/baseurl.js
@@ -0,0 +1,37 @@
+
+var baseurl =  "http://real-think.jmwl51.com";
+ var token=getCookie("token");  
+ 
+function setCookie(cname,cvalue,exdays=0){
+    var d = new Date();
+    d.setTime(d.getTime()+(exdays*24*60*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = cname+"="+cvalue+"; "+expires;
+}
+function getCookie(cname){
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+    }
+    return "";
+}
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+console.log(token);
+// retu;
+if(token==""){
+    window.location.href="./login.html" ;
+    
+}
+
+    
